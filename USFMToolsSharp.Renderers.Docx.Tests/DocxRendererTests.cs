9411cf1f0a32771fa571d143953add8d15a932c7
Craig Oliver <craig.robert.oliver@gmail.com>
2020-03-02 11:01:50 -0500

    Broke out tests that need refreshed renderer; remove pseudo-main

--- USFMToolsSharp.Renderers.Docx.Tests/DocxRendererTests.cs
+++ USFMToolsSharp.Renderers.Docx.Tests/DocxRendererTests.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NPOI.XWPF.UserModel;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using USFMToolsSharp.Models.Markers;
 
 namespace USFMToolsSharp.Renderers.Docx.Tests
@@ -26,10 +22,21 @@ namespace USFMToolsSharp.Renderers.Docx.Tests
         public void TestHeaderRender()
         {
             Assert.AreEqual("Genesis",renderDoc("\\h Genesis").Paragraphs[0].Text);
+        }
+        
+        [TestMethod]
+        public void TestHeaderRenderTwoWords()
+        {
             Assert.AreEqual("1 John", renderDoc("\\h 1 John").Paragraphs[0].Text);
+        }
+        
+        [TestMethod]
+        public void TestHeaderRenderBlank()
+        {
             Assert.AreEqual("", renderDoc("\\h      ").Paragraphs[0].Text);
 
         }
+
         [TestMethod]
         public void TestChapterRender()
         {
@@ -38,6 +45,7 @@ namespace USFMToolsSharp.Renderers.Docx.Tests
             Assert.AreEqual("-1", renderDoc("\\c -1").Paragraphs[0].Text);
             Assert.AreEqual("0", renderDoc("\\c 0").Paragraphs[0].Text);
         }
+
         [TestMethod]
         public void TestVerseRender()
         {
@@ -45,31 +53,12 @@ namespace USFMToolsSharp.Renderers.Docx.Tests
             Assert.AreEqual("1This is a simple verse.2Another one.", renderDoc("\\c 1 \\v 1 This is a simple verse. \\v 2 Another one.").Paragraphs[1].ParagraphText);
             Assert.AreEqual("2Another one.", renderDoc("\\c 1 \\v 1 This is a simple verse. \\c 2 \\v 2 Another one.").Paragraphs[3].ParagraphText);
         }
+
         [TestMethod]
         public void TestFootnoteRender()
         {
             Assert.AreEqual("1Hello Friend", renderDoc("\\c 1 \\v 1 This is a simple verse. \\f + \\ft Hello Friend \\f*").Paragraphs[3].ParagraphText);
             Assert.AreEqual("1Hello Fried Friend", renderDoc("\\c 1 \\v 1 This is a simple verse. \\f + \\ft \\fqa Hello Fried Friend \\f*").Paragraphs[3].ParagraphText);
-        }
-
-        [TestMethod]
-        public void TestCraigMain()
-        {
-            parser = new USFMParser(new List<string> { "s5", "fqa*" });
-            string inputFilename = @"C:\Users\oliverc.WAOFFICE\Downloads\docx-testing\1JN_2JN_3JN.usfm";
-            string usfm = File.ReadAllText(inputFilename);
-            USFMDocument markerTree = parser.ParseFromString(usfm);
-            DocxConfig config = new DocxConfig();
-            //config.separateChapters = true;
-            render = new DocxRenderer(config);
-            XWPFDocument testDoc = render.Render(markerTree);
-
-            string outputFilename = @"C:\Users\oliverc.WAOFFICE\Downloads\docx-testing\out.docx";
-            using (FileStream fs = File.OpenWrite(outputFilename))
-            {
-                testDoc.Write(fs);
-            }
-
         }
 
         public XWPFDocument renderDoc(string usfm)
