3c80c4e42142f22a2b81541572a338d18701bd63
Tony Anh Tran <34975907+AnonymousWalker@users.noreply.github.com>
2021-06-24 15:19:33 -0400

    Paragraph Indent Config (#31)
    * indented margin - applies to paragraphs
    
    * constructor default value

--- USFMToolsSharp.Renderers.Docx/DocxConfig.cs
+++ USFMToolsSharp.Renderers.Docx/DocxConfig.cs
@@ -7,6 +7,8 @@ namespace USFMToolsSharp.Renderers.Docx
 
         public ParagraphAlignment textAlign;
         public bool rightToLeft;
+        public int marginLeft; // cm unit
+        public int marginRight; // cm unit
         public string rightToLeftLangCode;
         public int columnCount;
         public double lineSpacing;
@@ -21,12 +23,19 @@ namespace USFMToolsSharp.Renderers.Docx
         {
             textAlign = ParagraphAlignment.LEFT;
             rightToLeft = false;
+            marginLeft = 0;
+            marginRight = 0;
             columnCount = 1;
             lineSpacing = 1;
             showPageNumbers = true;
             fontSize = 12;
         }
-        public DocxConfig(int fontSize = 12, bool separateChapters = false, bool separateVerses = false, bool showPageNumbers = true) : this()
+        public DocxConfig(
+            int fontSize = 12,
+            bool separateChapters = false, 
+            bool separateVerses = false, 
+            bool showPageNumbers = true
+        ) : this()
         {
             this.fontSize = fontSize;
             this.separateChapters = separateChapters;
---
+++ USFMToolsSharp.Renderers.Docx/Extensions/UnitValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USFMToolsSharp.Renderers.Docx.Extensions
+{
+    public static class UnitValue
+    {
+        public const int TWIP_PER_CM = 567;
+    }
+}
